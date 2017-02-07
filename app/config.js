ecb1d188191af075c042009a4fca79cf1450a425
Sorrel Brigman <sorrel.k@gmail.com>
2017-02-07 10:30:06 -0600

    added firebase dependency and basic form function

--- app/config.js
+++ app/config.js
@@ -1,5 +1,19 @@
 console.log("hello")
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDfUoz8OPRyS_8X-4oxAjkJ3SIMHgmKoRc",
+  authDomain: "skb-capstone-frontend.firebaseapp.com",
+  databaseURL: "https://skb-capstone-frontend.firebaseio.com",
+  storageBucket: "skb-capstone-frontend.appspot.com",
+  messagingSenderId: "1014795416567"
+};
+firebase.initializeApp(config);
+
+
+
+
+
 //assign the angular module to the var app
 var app = angular.module("forUsByUSApp", ['ngRoute'])
 
@@ -23,6 +37,24 @@ app.config(($routeProvider)=> {
 .controller('homeCtrl',  function(){
 
 })
-.controller('addProductCtrl', function() {
+.controller('addProductCtrl', function($scope) {
+
+  $scope.product = {
+    name: '',
+    company: '',
+    compWeb: '',
+    link: '',
+    image: '',
+    descript: '',
+    price: '',
+  }
+ $scope.addProduct = ()=> {
+    console.log("logging product")
+    console.log($scope.product)
+  }
+
+})
+.factory('addProductFactory', function(){
+  return {}
 
 })
