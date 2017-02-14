dbd47df24b0aea863e488adcc61bfa70f3e6d4b9
Sorrel Brigman <sorrel.k@gmail.com>
2017-02-14 13:27:29 -0600

    add product modal opens

--- client/app/controllers/amazonSearchCtrl.js
+++ client/app/controllers/amazonSearchCtrl.js
@@ -21,26 +21,27 @@ app.controller('amazonSearchCtrl',function($scope, $http){
         let searchResults = [];
         for (var i = 0; i < things.data.length; i++) {
           let searchProduct = {};
-          searchProduct.productTitle = things.data[i].ItemAttributes[0].Title[0];
-          console.log("title", searchProduct.productTitle);
-          searchProduct.productCompany = things.data[i].ItemAttributes[0].Brand[0];
-          console.log("company", searchProduct.productCompany);
+          searchProduct.category = $scope.amazonSearchQuery.searchIndex;
+          searchProduct.name = things.data[i].ItemAttributes[0].Title[0];
+          console.log("title", searchProduct.name);
+          searchProduct.company = things.data[i].ItemAttributes[0].Brand[0];
+          console.log("company", searchProduct.company);
 
           if(things.data[i].ImageSets) {
-            searchProduct.productImage = things.data[i].ImageSets[0].ImageSet[0].LargeImage[0].URL[0];
-            console.log("productImage", searchProduct.productImage);
+            searchProduct.image = things.data[i].ImageSets[0].ImageSet[0].LargeImage[0].URL[0];
+            console.log("productImage", searchProduct.image);
             if(things.data[i].ItemAttributes[0].ListPrice) {
-              searchProduct.listPrice = things.data[i].ItemAttributes[0].ListPrice[0].FormattedPrice[0];
-            console.log("price", searchProduct.listPrice);
+              searchProduct.price = things.data[i].ItemAttributes[0].ListPrice[0].FormattedPrice[0];
+            console.log("price", searchProduct.price);
             }
-            searchProduct.itemDescription = "";
+            searchProduct.descript = "";
           for (var j = 0; j < things.data[i].ItemAttributes[0].Feature.length; j++) {
-            searchProduct.itemDescription += things.data[i].ItemAttributes[0].Feature[j] + ". ";
+            searchProduct.descript += things.data[i].ItemAttributes[0].Feature[j] + ". ";
             };
-            console.log("description", searchProduct.itemDescription);
+            console.log("description", searchProduct.descript);
           }
-          searchProduct.productLink = things.data[i].DetailPageURL[0];
-          console.log("link", searchProduct.productLink);
+          searchProduct.link = things.data[i].DetailPageURL[0];
+          console.log("link", searchProduct.link);
           searchProduct.amazonUniqueId = things.data[i].ASIN[0];
           console.log("ASIN", searchProduct.amazonUniqueId);
           searchProduct.addDate = new Date();
@@ -49,6 +50,15 @@ app.controller('amazonSearchCtrl',function($scope, $http){
         };
         console.log("searchResults", searchResults);
         $scope.searchResults = searchResults;
+
+        $scope.addProduct = (product) => {
+           //activates the modal on the dynamically created content
+          $('.modal').modal();
+           //open the modal
+         $("#modal2").modal('open');
+         $scope.thisProduct = product;
+
+        };
     })
   }
 
@@ -58,93 +68,3 @@ app.controller('amazonSearchCtrl',function($scope, $http){
 
 
 });
-
-
-// my key AKIAJXXFIMAXVEXQ354Q
-//Signature key: CUzVp0CPbLCpbdTXUrmfnEZstpJ/6gbbCRnLsT9C
-
-// example amazon api call:
-
-// http://webservices.amazon.com/onca/xml?Service=AWSECommerceService&
-//         Operation=ItemSearch&
-//         AWSAccessKeyId=AKIAJXXFIMAXVEXQ354Q&
-//         AssociateTag=sorrelbrigman-20&
-//         Keywords=Harry%20Potter&
-//         ResponseGroup=Images%2CItemAttributes%2COffers&
-//         SearchIndex=Books&
-//         Timestamp=2015-08-11T17%3A51%3A56.000Z&
-//         Signature=w1cntxdHX+NuiaEBSFKweQIShxurfiZq/z42I72K
-
-
-
-//   let today = new Date();
-//   let todayISO = today.toISOString();
-//   let urlTimeStamp = escape(todayISO);
-
-
-// //node code from stackoverflow
-// var request = require("request");
-// var crypto = require("crypto");
-// var d = new Date().toUTCString();
-// var config = {
-//   awsAccessKeyId : "AKIAJXXFIMAXVEXQ354Q",
-//   awsSecretKey   : "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"
-// };
-
-// // Options for the http POST request
-// var options = {
-//   "headers" : {
-//     "Date"   : d, // Must pass the Date header as well as the X-Amzn-Authorization header
-//     "X-Amzn-Authorization" : "AWS3-HTTPS AWSAccessKeyId=" + config.awsAccessKeyId +
-//       ",Algorithm=HMACSHA256,Signature=" + generateHmac(d, config.awsSecretKey)
-//   },
-//   "form"    : {
-//   }
-// };
-
-
-// function generateHmac (data, awsSecretKey, algorithm, encoding) {
-//   encoding = encoding || "base64";
-//   algorithm = algorithm || "sha256";
-//   return crypto.createHmac(algorithm, awsSecretKey).update(data).digest(encoding);
-// }
-
-
-// var amazon = require('amazon-product-api');
-
-// var client = amazon.createClient({
-//   awsId: "AKIAJXXFIMAXVEXQ354Q",
-//   awsSecret: "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",
-//   awsTag: "sorrelbrigman-20"
-// });
-
-// client.itemSearch({
-//   director: 'Quentin Tarantino',
-//   actor: 'Samuel L. Jackson',
-//   searchIndex: 'DVD',
-//   audienceRating: 'R',
-//   responseGroup: 'ItemAttributes,Offers,Images'
-// }).then(function(results){
-//   console.log(results);
-// }).catch(function(err){
-//   console.log(err);
-// });
-
-
-
-
-
-
-
-
-// $http.get(`http://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAIQAVHDMK5HGBAEEA&AssociateTag=sorrelbrigman&Keywords=pi%C3%B1on%20coffee&Operation=ItemSearch&ResponseGroup=Images%2CItemAttributes%2COfferSummary&SearchIndex=Grocery&Service=AWSECommerceService&Timestamp=2017-02-13T15%3A37%3A16.000Z&Signature=CDD8F1%2FBiBaY0%2FIr6R2E8dv7HCt3vP1Vl8Tq9yXxVEM%3D`)
-//   .then((e)=>{
-//     console.log("e", e);
-//   })
-//   .catch((e)=>{
-//     console.log("error", e);
-//   });
-
-
-
-//QcEoo9MqKiokQKAjr6mMfhotHgQmerco3AHZvYizPEM%3D
