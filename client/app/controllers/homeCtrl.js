32e634306787e21ea11cb358b85d1dab01c2d0ad
Sorrel Brigman <sorrel.k@gmail.com>
2017-02-15 16:54:14 -0600

    gives user toast to log in for flagging

--- client/app/controllers/homeCtrl.js
+++ client/app/controllers/homeCtrl.js
@@ -16,9 +16,17 @@ app.controller('homeCtrl',  function($scope, products, getProductFactory, $route
   };
 
   $scope.flagItem = (value)=> {
+    authFactory.getUser()
+    .then((e) => {
+      let whichProduct = value;
+      $location.url(`/flag/${whichProduct}`);
+    })
+    .catch(()=>{
+       //if they are not logged in
+        Materialize.toast("Please log in to flag an item!", 4000, 'round right'); // 4000 is the duration of the toast
+          //don't allow them to flag
 
-    let whichProduct = value;
-    $location.url(`/flag/${whichProduct}`);
+    });
 
   };
 
