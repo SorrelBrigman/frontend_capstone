7add32ed606b120e0ac516bf2b5e796e4c8def5d
Sorrel Brigman <sorrel.k@gmail.com>
2017-02-27 10:09:14 -0600

    Add config to git

---
+++ client/config/config.js
@@ -0,0 +1,8 @@
+"use strict"
+
+module.exports = {
+  key: process.env.AWS_KEY || 'ABCDEFGH1234567890AB',
+  secret: process.env.AWS_SECRET || 'YaTb2VDWSsm4P3u98tzFY9Bcb4YqSSSWNAPjMZyh'
+}
+
+console.log("the config!!!");
